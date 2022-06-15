1b42e9dd920a45a40704d8ec28dfd2dbbf51cb2e
DarccMan <darcy3057@gmail.com>
2022-06-15 10:56:41 +1000

    Fix touch

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
+// Stop context menu
+window.oncontextmenu = function (event) {
+  event.preventDefault();
+  event.stopPropagation();
+  return false;
+};
+
+// Change to yellow after time
 const time = 2000; // Time until turns angry
 var timer;
 addEventListener("mousedown", () => {
+  timer = setTimeout(makeAngry, time);
+});
+addEventListener("touchstart", () => {
   timer = setTimeout(makeAngry, time);
 });
 addEventListener("mouseup", () => {
   clearTimeout(timer);
   makeNotAngry();
 });
-
+addEventListener("touchend", () => {
+  clearTimeout(timer);
+  makeNotAngry();
+});
+addEventListener("touchcancel", () => {
+  clearTimeout(timer);
+  makeNotAngry();
+});
 function makeAngry() {
   $(".contain").addClass("angry");
 }
 function makeNotAngry() {
   $(".contain").removeClass("angry");
 }
-
-window.oncontextmenu = function(event) {
-  event.preventDefault();
-  event.stopPropagation();
-  return false;
-};
