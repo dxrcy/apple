a10e530d2887e5ce9de4c0a5d787db5db50659aa
darcy <acednes@gmail.com>
2024-02-04 12:01:21 +1100

    format files

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-// Please stop trying to STEAL our SECRET source code for this innovative website!
+/* Please stop trying to STEAL our SECRET source code for this innovative website! */
 
 // Stop context menu
 window.oncontextmenu = function (event) {
-  event.preventDefault();
-  event.stopPropagation();
-  return false;
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
 };
 
 // Change to yellow after time
 const time = 2000; // Time until turns angry
 var timer;
 addEventListener("mousedown", () => {
-  timer = setTimeout(makeAngry, time);
+    timer = setTimeout(makeAngry, time);
 });
 addEventListener("touchstart", () => {
-  timer = setTimeout(makeAngry, time);
+    timer = setTimeout(makeAngry, time);
 });
 addEventListener("mouseup", () => {
-  clearTimeout(timer);
-  makeNotAngry();
+    clearTimeout(timer);
+    makeNotAngry();
 });
 addEventListener("touchend", () => {
-  clearTimeout(timer);
-  makeNotAngry();
+    clearTimeout(timer);
+    makeNotAngry();
 });
 addEventListener("touchcancel", () => {
-  clearTimeout(timer);
-  makeNotAngry();
+    clearTimeout(timer);
+    makeNotAngry();
 });
 function makeAngry() {
-  $(".contain").addClass("angry");
+    $(".contain").addClass("angry");
 }
 function makeNotAngry() {
-  $(".contain").removeClass("angry");
+    $(".contain").removeClass("angry");
 }
